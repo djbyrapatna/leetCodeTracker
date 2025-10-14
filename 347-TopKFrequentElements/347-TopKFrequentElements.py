9459e2958944f624cd99afee09ee94a7b3b85be5
Dhruva J. Byrapatna <dhruvajb@gmail.com>
2025-10-14 17:42:29 -0400

    File updated at 10/14/2025, 5:42:29 PM

--- 347-TopKFrequentElements/347-TopKFrequentElements.py
+++ 347-TopKFrequentElements/347-TopKFrequentElements.py
@@ -1,22 +1,25 @@
-# Last updated: 10/14/2025, 3:03:28 PM
+# Last updated: 10/14/2025, 5:42:29 PM
 import heapq
-from collections import defaultdict
+from collections import defaultdict, Counter
 
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
         pq = []
-        heapq.heapify(pq)
-        dc = defaultdict(int)
-        for num in nums:
-            dc[num]+=1
-        for key in dc.keys():
-            val = dc[key]
-            heapq.heappush(pq,(-val,key))
+        #heapq.heapify(pq)
+        ctr = Counter(nums)
+        mc = ctr.most_common(k)
+        return [x for [x,y] in mc]
+        #dc = defaultdict(int)
+        # for num in nums:
+        #     dc[num]+=1
+        # for key in dc.keys():
+        #     val = dc[key]
+        #     heapq.heappush(pq,(-val,key))
 
-        retList = [0]*k
-        for i in range(k):
-            _, elem = heapq.heappop(pq)
-            retList[i] = elem
-        return retList
+        # retList = [0]*k
+        # for i in range(k):
+        #     _, elem = heapq.heappop(pq)
+        #     retList[i] = elem
+        # return retList
         
         
